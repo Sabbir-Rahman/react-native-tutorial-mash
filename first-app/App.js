aed058726746a1cc74815621316bdb699b90a361
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-04-07 02:10:07 +0600

    flat list

--- first-app/App.js
+++ first-app/App.js
@@ -1,45 +1,60 @@
 import React, { useState } from 'react';
-import { Button, Linking, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, Linking, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [Items, setItems] = useState([
-    { key: 1, item: 'Item 1' },
-    { key: 2, item: 'Item 2' },
-    { key: 3, item: 'Item 3' },
-    { key: 4, item: 'Item 4' },
-    { key: 5, item: 'Item 5' },
-    { key: 6, item: 'Item 6' },
-    { key: 7, item: 'Item 7' },
-    { key: 8, item: 'Item 8' },
-    { key: 9, item: 'Item 9' }
+    { item: 'Item 1' },
+    { item: 'Item 2' },
+    { item: 'Item 3' },
+    { item: 'Item 4' },
+    { item: 'Item 5' },
+    { item: 'Item 6' },
+    { item: 'Item 7' },
+    { item: 'Item 8' },
+    { item: 'Item 9' }
   ]) 
 
   const [Refreshing, setRefreshing] = useState(false)
   const onRefres = () => {
     setRefreshing(true)
-    setItems([...Items, { key: 69, item: 'Item 69'}])
+    setItems([...Items, { item: 'Item 69'}])
     setRefreshing(false)
   }
 
   return (
-    <View style={styles.container}>
-      <ScrollView
-        refreshControl={
-          <RefreshControl
-            refreshing={Refreshing}
-            onRefresh={onRefres}
-          />
-        }
-      >
-        {Items.map(i => {
-          return (
-            <View style={styles.item}>
-              <Text style={styles.text}>{i.item}</Text>
-            </View>
-          )
-        })}
-      </ScrollView>
-    </View>
+    <FlatList
+      data={Items}
+      keyExtractor={(item,index) => index.toString()}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefres}
+        />
+      }
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.item}</Text>
+        </View>
+      )}
+    />
+    // <View style={styles.container}>
+    //   <ScrollView
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={Refreshing}
+    //         onRefresh={onRefres}
+    //       />
+    //     }
+    //   >
+    //     {Items.map(i => {
+    //       return (
+    //         <View style={styles.item}>
+    //           <Text style={styles.text}>{i.item}</Text>
+    //         </View>
+    //       )
+    //     })}
+    //   </ScrollView>
+    // </View>
   )
 }
 
